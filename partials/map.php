666bd6781e658c23d04a6def278e1570875d0ab0
SEAN <seane235@gmail.com>
2015-08-13 15:27:46 -0400

    sass partial for blog, education section

---
+++ partials/map.php
@@ -0,0 +1,5 @@
+<div class="row" id="map-row">
+	<div class="content-wrapper">
+		<?php get_template_part('img/svg', 'usa2'); ?>
+	</div>
+</div>
---
+++ tmpl/tmpl-blog.php
@@ -0,0 +1,38 @@
+<?php /* Template Name: Blog Listing */ ?>
+
+
+<?php get_header(); ?>
+<?php get_template_part('partials/hero'); ?>
+<?php get_template_part('partials/navbar'); ?>
+<div id="blog-list">
+	<div class="content-wrapper">
+	<div class="blog-wrapper">
+		<div class="heading-wrapper">
+			<h2>Blog</h2>
+			<div class="subheading">Web Development | Web Design</div>
+		</div>
+
+		<?php $counter = 1; $posts = get_posts(array(
+			'posts_per_page' => '10',
+		)); foreach($posts as $post ) : setup_postdata($post); ?>
+		<div class="blog-post">
+			<a href="<?php the_permalink(); ?>">
+				<div class="counter">
+					<hr>
+					<div class="num"><?php echo $counter; ?></div>
+				</div>
+				<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); ?>
+				<div style="background-image: url('<?php echo $url ?>');" class="thumb-wrapper">
+				<h4><?php $title = $post->post_title; if(!empty($title)){echo $title;} ?></h4>
+				</div>
+			</a>
+			<div class="excerpt">
+				<?php the_excerpt(); ?>
+			</div>
+		</div>
+	<?php $counter++; endforeach; ?>
+		
+	</div>
+	</div>
+</div>
+<?php get_footer(); ?>
